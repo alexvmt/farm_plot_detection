cc078a1fb5cf5f83a53c6fea5753a5ee339da510
alexvmt <alexandermerdiantarko@gmail.com>
2022-05-05 21:43:53 +0200

    google earth engine farm plots and sentinel-2 overlay

---
+++ gee_farm_plots_and_sentinel_2_overlay.js
@@ -0,0 +1,43 @@
+/**
+ * Function to mask clouds using the Sentinel-2 QA band
+ * @param {ee.Image} image Sentinel-2 image
+ * @return {ee.Image} cloud masked Sentinel-2 image
+ */
+function maskS2clouds(image) {
+  var qa = image.select('QA60');
+
+  // Bits 10 and 11 are clouds and cirrus, respectively.
+  var cloudBitMask = 1 << 10;
+  var cirrusBitMask = 1 << 11;
+
+  // Both flags should be set to zero, indicating clear conditions.
+  var mask = qa.bitwiseAnd(cloudBitMask).eq(0)
+      .and(qa.bitwiseAnd(cirrusBitMask).eq(0));
+
+  return image.updateMask(mask).divide(10000);
+}
+
+var dataset = ee.ImageCollection('COPERNICUS/S2_SR')
+                  .filterDate('2020-04-01', '2020-04-30') // set date range according to selected year below
+                  // Pre-filter to get less cloudy granules.
+                  .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE',10))
+                  .map(maskS2clouds);
+
+var visualization = {
+  min: 0.0,
+  max: 0.3,
+  bands: ['B4', 'B3', 'B2'],
+};
+
+Map.setCenter(23.4, -16.5, 8);
+
+Map.addLayer(dataset.mean(), visualization, 'RGB');
+
+var farm_plots = ee.FeatureCollection("users/alexvmt/farm_plots")
+                    .filter('Year == 2020'); // filter year according to selected date range above
+
+Map.addLayer(farm_plots);
+
+var styling = {color: 'red', fillColor: '00000000'};
+
+Map.addLayer(farm_plots.style(styling));
