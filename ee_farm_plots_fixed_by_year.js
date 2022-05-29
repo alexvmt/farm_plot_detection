997ea14032c5569232db95a44a8bcfbf09df4883
alexvmt <alexandermerdiantarko@gmail.com>
2022-05-29 12:48:28 +0200

    analyze fixed locations and sample regions

---
+++ ee_farm_plots_fixed_by_year.js
@@ -0,0 +1,23 @@
+var farm_plots = ee.FeatureCollection("users/alexvmt/farm_plots_fixed_locations");
+
+Map.setCenter(23.4, -17, 9);
+
+var years = ee.Dictionary({
+  2017: {color: 'green', fillColor: '00000000'},
+  2018: {color: 'red', fillColor: '00000000'},
+  2019: {color: 'blue', fillColor: '00000000'},
+  2020: {color: 'orange', fillColor: '00000000'},
+});
+
+function map_style_to_years(feature) {
+  return feature.set('style', years.get(feature.get('year')));
+}
+
+farm_plots = farm_plots.map(map_style_to_years);
+
+var farm_plots_vis = farm_plots.style({
+  styleProperty: 'style',
+  neighborhood: 8,
+});
+
+Map.addLayer(farm_plots_vis, null, 'Farm plots fixed locations by year');
