0372aa0666168fe0151d50a3f833c206ecec29a4
alexvmt <alexandermerdiantarko@gmail.com>
2022-12-12 22:10:15 +0100

    evaluate region-specific thresholds

--- earth_engine_scripts/evaluate_classification_thresholds.js
+++ earth_engine_scripts/evaluate_classification_thresholds.js
@@ -21,46 +21,61 @@ function maskS2clouds(image) {
 
 
 // Load data
-var openmapflow_crop_map_sichifulo_2020 = ee.Image('users/alexvmt/openmapflow_crop_map_sichifulo_2020');
+
+//var kaza_bengo_crop_2020 = ee.FeatureCollection('projects/ee-alexvmt/assets/kaza_bengo_crop_2020_random_2000');
 var kaza_bengo_crop_2020 = ee.FeatureCollection('projects/ee-alexvmt/assets/kaza_bengo_crop_2020_random_20000');
-var roi = ee.FeatureCollection('projects/ee-alexvmt/assets/Sichifulo');
+
+var roi = ee.FeatureCollection('projects/ee-alexvmt/assets/NAM_sites_bengo');
+//var roi = ee.FeatureCollection('projects/ee-alexvmt/assets/ZAM_sites_bengo');
+//var roi = ee.FeatureCollection('projects/ee-alexvmt/assets/ZIM_sites_bengo');
+
+var openmapflow_crop_map_2020 = ee.Image('projects/ee-alexvmt/assets/NAM_sites_bengo_crop_map_2020');
+//var openmapflow_crop_map_2020 = ee.Image('projects/ee-alexvmt/assets/ZAM_sites_bengo_crop_map_2020');
+//var openmapflow_crop_map_2020 = ee.Image('projects/ee-alexvmt/assets/ZIM_sites_bengo_crop_map_2020');
 
 
 
-// Export actual labels
+// Export actual train and test labels within roi
 var actual = kaza_bengo_crop_2020.filterBounds(roi);
 
 Export.table.toDrive({
   collection: actual,
-  description: 'actual',
-  folder: 'farm_plot_detection',
+  description: 'export_actual_labels',
+  folder: 'NAM',
+  //folder: 'ZAM',
+  //folder: 'ZIM',
+  fileNamePrefix: 'actual_labels'
   fileFormat: 'CSV'
   });
 
 
 
-// OpenMapflow
+// OpenMapflow model
 
 // Define thresholds
 var thresholds = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1];
 
-// Export predicted labels for different thresholds
+// Export predicted labels for different thresholds within roi
 thresholds.forEach(function (threshold) {
   
-  var prediction = openmapflow_crop_map_sichifulo_2020.gt(threshold).reduceToVectors({
-    geometry: actual,
-    scale: 10,
-    geometryType: 'centroid',
-    labelProperty: 'prediction',
-    maxPixels: 100e9
-    });
+  var prediction = openmapflow_crop_map_2020
+	.gt(threshold)
+	.reduceToVectors({
+		geometry: actual,
+		scale: 10,
+		geometryType: 'centroid',
+		labelProperty: 'prediction',
+		maxPixels: 100e9
+		});
   
-  var file_name = 'openmapflow_threshold_' + threshold.toString().replace('.', '_');
+  var file_name = 'openmapflow_predictions_threshold_' + threshold.toString().replace('.', '_');
   
   Export.table.toDrive({
     collection: prediction,
-    description: 'prediction',
-    folder: 'farm_plot_detection',
+    description: 'export_openmapflow_predictions',
+    folder: 'NAM',
+    //folder: 'ZAM',
+    //folder: 'ZIM',
     fileNamePrefix: file_name,
     fileFormat: 'CSV'
     });
@@ -70,9 +85,6 @@ thresholds.forEach(function (threshold) {
 
 
 // Random forest
-
-// Get points within roi
-var points = kaza_bengo_crop_2020.filterBounds(roi);
 
 // Select Sentinel-2 images
 var s2_images = ee.ImageCollection('COPERNICUS/S2_SR')
@@ -98,30 +110,36 @@ var s2_image = s2_images
 var bands = ['B2', 'B3', 'B4', 'B8', 'NDVI'];
 
 // Train test split
-var train_points = points.filter('subset == "train"');
-var test_points = points.filter('subset == "test"');
+var train_points = kaza_bengo_crop_2020.filter('subset == "train"');
+var test_points = kaza_bengo_crop_2020.filter('subset == "test"');
 
 // Create train data
-var train = s2_image.clip(roi).select(bands).sampleRegions({
-	collection: train_points,
-	properties: ['crop'],
-	scale: 10
-	});
+var train = s2_image
+  .select(bands)
+  .sampleRegions({
+    collection: train_points,
+    properties: ['crop'],
+    scale: 10
+    });
 
-// Create test data
-var test = s2_image.clip(roi).select(bands).sampleRegions({
-	collection: test_points,
-	properties: ['crop'],
-	scale: 10
-	});
+// Create test data within roi
+var test = s2_image
+  .clip(roi)
+  .select(bands)
+  .sampleRegions({
+    collection: test_points,
+    properties: ['crop'],
+    scale: 10
+    });
 
 // Train classifier
-var classifier = ee.Classifier.smileRandomForest(10).train({
-	features: train,
-	classProperty: 'crop',
-	inputProperties: bands
-	})
-	.setOutputMode('PROBABILITY');
+var classifier = ee.Classifier.smileRandomForest(10)
+  .train({
+    features: train,
+    classProperty: 'crop',
+    inputProperties: bands
+    })
+  .setOutputMode('PROBABILITY');
 
 // Classify test set
 var test_pred = test.classify(classifier);
@@ -129,8 +147,10 @@ var test_pred = test.classify(classifier);
 // Export predicted class probabilities
 Export.table.toDrive({
     collection: test_pred,
-    description: 'class_probability',
-    folder: 'farm_plot_detection',
+    description: 'export_random_forest_class_probabilities',
+    folder: 'NAM',
+    //folder: 'ZAM',
+    //folder: 'ZIM',
     fileNamePrefix: 'random_forest_class_probabilities',
     fileFormat: 'CSV'
   });
